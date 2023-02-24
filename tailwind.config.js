546699869db13ce88b6add6f7603f72d495bd04c
Marcos <marcoswodbarros@outlook.com>
2023-02-24 20:03:37 -0300

    Adding colors, fonts and sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: '#dce4e3',
+        green: '#18c29c',
+        lightGreen: '#8ed7c6',
+        lightGrey: '#dce4e3',
+        textColor: '#084236',
+      },
+
+      fontFamily: {
+        poppins: 'Poppins',
+      },
+
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 }
