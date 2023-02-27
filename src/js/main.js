91a52ca3857f8ee7da7410a3cbc656067f8eb863
Marcos <marcoswodbarros@outlook.com>
2023-02-27 10:40:29 -0300

    Updating the logout

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 let openSide = document.querySelector('#menu__closed');
 let closeSide = document.querySelector('#menu__opened');
 let vetLogo = document.querySelector('#vet__logo');
+let nav = document.querySelector('nav');
 let liText1 = document.querySelector('.li__text--first');
 let liText2 = document.querySelector('.li__text--second');
 let liText3 = document.querySelector('.li__text--third');
@@ -8,6 +9,7 @@ let liText4 = document.querySelector('.li__text--fourth');
 let liText5 = document.querySelector('.li__text--fifth');
 let userPic = document.querySelector('#user__pic');
 let userInfo = document.querySelector('.user__info');
+let logout = document.querySelector('#logout');
 
 openSide.addEventListener("click", openMenu);
 closeSide.addEventListener("click", closeMenu);
@@ -16,6 +18,7 @@ function openMenu () {
     openSide.style.display = "none";
     closeSide.style.display = "block";
     vetLogo.style.display = "block";
+    nav.style.width = "25rem";
     liText1.style.display = "inline";
     liText2.style.display = "inline";
     liText3.style.display = "inline";
@@ -23,12 +26,14 @@ function openMenu () {
     liText5.style.display = "inline";
     userPic.style.display = "block";
     userInfo.style.display = "block";
+    logout.style.marginLeft = "2rem";
 }
 
 function closeMenu () {
     openSide.style.display = "block";
     closeSide.style.display = "none";
     vetLogo.style.display = "none";
+    nav.style.width = "auto";
     liText1.style.display = "none";
     liText2.style.display = "none";
     liText3.style.display = "none";
@@ -36,4 +41,5 @@ function closeMenu () {
     liText5.style.display = "none";
     userPic.style.display = "none";
     userInfo.style.display = "none";
+    logout.style.marginLeft = "auto";
 }
